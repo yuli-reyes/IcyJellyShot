089debb17c17d200eb4cb2467f5904cc19369240
yuli-reyes <viviana1236@gmail.com>
2021-06-23 22:09:55 -0500

    Se agregó el componente nav y el footer, en el App.js estan los componentes Nav y Footer para visualizarlos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+
+
+
+
+
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
 
-function App() {
+function App(){
   return (
     <div>
+      <Nav />      
+      <Footer />
 
     </div>
   );

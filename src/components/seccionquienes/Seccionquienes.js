e42e97803b5bbad8e0986b3e48a6607561af5ac0
yuli-reyes <viviana1236@gmail.com>
2021-06-25 19:44:10 -0500

    Se agregó componente coctel del mes para el home

--- src/components/seccionquienes/Seccionquienes.js
+++ src/components/seccionquienes/Seccionquienes.js
@@ -3,9 +3,9 @@ import imgquienes from './imgquienes.jpg'
 
 function Seccionquienes(){ return(
   <div>
-      <div className="container">
+      <div className="container py-5">
                 <div className="row">
-                    <div className="cajaquienes col-lg-6">
+                    <div className="cajaquienes col-12 col-md-6">
                         <h1 className="mb-4">QUIÉNES SOMOS</h1>
                         <p>Somos  <strong>Icy  Jelly Shot </strong>trabajamos por la calidad de nuestras 
                            bebidas congeladas elaboradas para todos los gustos. Contamos con dos alternativas, 
@@ -17,7 +17,7 @@ function Seccionquienes(){ return(
                         </p>
                     </div>
                     
-                    <div className="col-lg-6"><img className="img-fluid rounded" src={imgquienes} alt="imagenQuienesSomos" />
+                    <div className="col-12 col-md-6"><img className="img-fluid rounded" src={imgquienes} alt="imagenQuienesSomos" />
                     </div>
 
                 </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,15 @@
 import './home.css';
-<<<<<<< HEAD
-import Nav from './components/nav/Nav';
-import Footer from './components/footer/Footer';
+import Nav from '../../components/nav/Nav';
+import Footer from '../../components/footer/Footer';
 import Seccionquienes from '../../components/seccionquienes/Seccionquienes';
+import Seccioncoctelmes from '../../components/seccioncoctelmes/Seccioncoctelmes';
 
 function Home() {
 	return(
 		<div>
 		    <Nav />
-		    <Seccionquienes />   
+		    <Seccionquienes />
+		    <Seccioncoctelmes />
       		<Footer />
 		</div>
 

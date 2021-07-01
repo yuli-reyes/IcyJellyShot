ac1fadd3d0eb0270ffebd73995381b0824d40e2b
mariacamilaleon <sra.magohd@gmail.com>
2021-07-01 00:08:58 -0500

    Se sube el componente de seccion cocteles de la página cátalogo y su respectivos modales

--- src/pages/catalogo/Catalogo.js
+++ src/pages/catalogo/Catalogo.js
@@ -1,8 +1,15 @@
+import Nav from '../../components/nav/Nav';
+import Footer from '../../components/footer/Footer';
+import Seccioncocteles from '../../components/seccioncocteles/Seccioncocteles';
+
 function Catalogo() {
 	return(
 		<div>
+		    <Nav />
+		    <Seccioncocteles />
+      		<Footer />
 		</div>
-		);
 
+		);
 }
 export default Catalogo;

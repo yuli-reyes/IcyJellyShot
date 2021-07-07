6c8ebbf8bc74ed6c7b0ef7672cfbcf53786f4e52
yuli-reyes <viviana1236@gmail.com>
2021-07-07 16:53:47 -0500

    Se cambiaron los id de smoothies

--- src/components/modalsmoothies/modalsmoothies.js
+++ src/components/modalsmoothies/modalsmoothies.js
@@ -14,37 +14,37 @@ function ModalSmoothie(props) {
                         <div className="row">
                           <div className="col-sm-12">
                         
-                            <div className="row">
-                              <div className="imagen-g col-sm-6 col-md-6 col-lg-6">
-                                 <img src={props.pms3} width="250px" />
-                              </div>
-                              <div className="texticon col-sm-6 col-md-6 col-lg-6">
-                                    <h3 className="subtitulom">Descripción</h3>
-                                    <p className="pmodal">{props.pms4}</p>
-                                  <div className="modal-iconos row">
-                                
-                                        <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
-                                            <img src={props.pms5}/>
-                                            <h5>{props.pms8}</h5>
-                                            <h5>{props.pms9}</h5>   
-                                        </div>
-    
-                                        <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
-                                            <img src={props.pms6}/>
-                                            <h5>{props.pms10}</h5>
-                                            <h5>{props.pms11}</h5>
-                                            
-                                        </div>
-    
-                                        <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
-                                            <img src={props.pms7}/>
-                                            <h5>{props.pms12}</h5>
-                                            <h5>{props.pms13}</h5>
-                                            
-                                        </div>
-                                </div>   
-                              </div>
-                            </div>
+                                <div className="row">
+                                  <div className="imagen-g col-sm-6 col-md-6  col-lg-6">
+                                     <img src={props.pms3} width="250px" />
+                                  </div>
+                                  <div className="texticon col-sm-6 col-md-6 col-lg-6">
+                                        <h3 className="subtitulom">Descripción</h3>
+                                        <p className="pmodal">{props.pms4}</p>
+                                        <div className="modal-iconos row">
+                                    
+                                            <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
+                                                <img src={props.pms5}/>
+                                                <h5>{props.pms8}</h5>
+                                                <h5>{props.pms9}</h5>   
+                                            </div>
+        
+                                            <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
+                                                <img src={props.pms6}/>
+                                                <h5>{props.pms10}</h5>
+                                                <h5>{props.pms11}</h5>
+                                                
+                                            </div>
+        
+                                            <div className="iconos-m col-sm-12 col-md-4 col-lg-4"> 
+                                                <img src={props.pms7}/>
+                                                <h5>{props.pms12}</h5>
+                                                <h5>{props.pms13}</h5>
+                                                
+                                            </div>
+                                        </div>   
+                                  </div>
+                                </div>
                           </div>
                         </div>
                     </div>
